18f9e55af9fde11cdb87acd6995512c5dc7ec5e6
Rodrigo Amaro <jbravo.br@gmail.com>
2016-09-16 00:55:23 -0300

    Commit Inicial

---
+++ AppTesteMITInfnet/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using Prism.Mvvm;
+using PropertyChanged;
+using System;
+using Xamarin.Forms;
+
+namespace AppTesteMITInfnet
+{
+    [ImplementPropertyChanged]
+    public class HomePageViewModel : BindableBase
+    {
+        public string Title { get; set; }
+        public DateTime Date { get; } = DateTime.Now;
+
+        public Command OlaCommand
+        {
+            get
+            {
+                return new Command<string>((string obj) => Navigate(obj));
+            }
+        }
+
+        async void Navigate(string value) 
+        {
+            var x = value;
+        }
+
+        public HomePageViewModel(string A, string B)
+        {
+        }
+    }
+}
---
+++ AppTesteMITInfnet/Views/Blablabla.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms;
+
+namespace AppTesteMITInfnet.Views
+{
+    public partial class Blablabla : ContentPage
+    {
+        public Blablabla()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
